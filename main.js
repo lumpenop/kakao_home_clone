e8249cbeae6822e2e2d401ab31925a4d87bbfe8a
swk <swk@swkui-Macmini.local>
2021-04-02 00:35:22 +0900

    header 수정

--- main.js
+++ main.js
@@ -10,7 +10,9 @@ document.addEventListener('scroll', function() {
 
       if(beforePosition < afterPosition ){
         document.querySelector(".header").style.background='black';
+        document.querySelector(".header").style.border='none';
         document.querySelector(".title_logo span").style.color='white';
+        
       } else {
         document.querySelector(".header").style.background='white';
         document.querySelector(".title_logo span").style.color='black';
