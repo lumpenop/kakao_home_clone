a82062b8f64ef5091a7a07e207a19ddf4f2b01c0
lumpenop <swk5373@gmail.com>
2021-04-05 19:41:20 +0900

    반응형, 하단 네비게이션 추가

--- main.js
+++ main.js
@@ -7,25 +7,38 @@ document.addEventListener('scroll', function() {
    
    
     if (afterPosition > 50) {
-
+      document.querySelector('.side_area input').checked = false;
+      
       if(beforePosition < afterPosition ){
         document.querySelector(".header").style.background='black';
         
         document.querySelector(".title_logo span").style.color='white';
-        document.querySelector(".header").style.zIndex = 0;
+        document.querySelector(".header").style.zIndex = 1;
+        ham = document.querySelectorAll(".side_area span")
+        document.querySelector('.side_area input').checked = false;
+        
+        for (i=0; i<ham.length; i++){
+          ham[i].style.background='white';
+        }
 
       } else {
+        document.querySelector('.side_area input').checked = false;
         document.querySelector(".header").style.background='white';
+        
         document.querySelector(".title_logo span").style.color='black';
-        document.querySelector(".header").style.zIndex = 1;
+        document.querySelector(".header").style.zIndex = 9999;
+        ham = document.querySelectorAll(".side_area span")
+        for (i=0; i<ham.length; i++){
+          ham[i].style.background='black';
+        }
       }
 
     } else {
-  
+      document.querySelector('.side_area input').checked = false;
       document.querySelector(".header").style.background='white';
       document.querySelector(".title_logo span").style.color='black';
       
-      document.querySelector(".header").style.zIndex = 1;
+      document.querySelector(".header").style.zIndex = 9999;
   
     }
     beforePosition = afterPosition;
@@ -36,6 +49,7 @@ document.addEventListener('scroll', function() {
 const changeHandler = () => {
   console.log(window.innerHeight);
   document.querySelector(".video > video").style.height=String(window.innerHeight)+"px";
+  document.querySelector("#side_flag").checked = false;
 };
 
 window.addEventListener("resize", changeHandler);
@@ -96,12 +110,77 @@ const io = new IntersectionObserver((entries, observer)=>{
      
     if (entry.intersectionRatio >= 1){
           entry.target.play();
-    } else if (entry.intersectionRatio >0.3) {
-        entry.target.pause();
-       
+    } else if (entry.intersectionRatio < 1 && entry.intersectionRatio >= 0.4){
+      entry.target.pause();
+    } else{
+        entry.target.load();
     }
     });},options);
 
 box = document.querySelector('.card_body > div > div > video');
 io.observe(box);
+
+
+blackHead = ()  => {
+  if(document.querySelector('.side_area input').checked){
+    console.log("checked")
+    document.querySelector('.header').style.background = 'black';
+   document.querySelector('.title_logo a > span').style.color = 'white';
+   span = document.querySelectorAll('.side_area span')
+    for(i=0; i < span.length; i++){
+      span[i].style.background = 'white';
+    }
+    document.querySelector('.header').style.zIndex = 9999;
+  } else {
+    console.log("un checked")
+    document.querySelector('.header').style.background = 'white';
+    document.querySelector('.title_logo a > span').style.color = 'black';
+    span = document.querySelectorAll('.side_area span')
+    for(i=0; i < span.length; i++){
+      span[i].style.background = 'black';
+    }
+
+  }
+}
+
+
+let index = 0;
+beforeCopy = '';
+liCopy = document.querySelectorAll('.ico_corp')
+
+for(i=0;i<liCopy.length;i++){
+  liCopy[i].parentElement.setAttribute('onclick','copyOn(this);');
+  
+}
+
+function copyOn(copyA){
+  
+
+  for(i=0; i < liCopy.length; i++){
+    if (liCopy[i].parentElement==copyA){
+      index = i-1;
+    }  
+  }
+  
+  ul = document.querySelectorAll('.info_copyright li ul')
+  
+  for(i=0; i < ul.length; i++){
+    if(index!=i){
+      ul[i].className = 'list_relation';
+    }
+  }
+
+  if(ul[index].className=='list_relation'){
+    ul[index].className = 'list_relation relation_on';
+ 
+  } else {
+    ul[index].className = 'list_relation';
+  
+  }
+  
+  
+}
+
+
+
 
